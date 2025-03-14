4eae3d16a5c330b2f89cf6d5fc80cf889c11be77
notnbhd <bachhaidangnguyen@gmail.com>
2025-03-14 23:19:41 +0700

    improve the model & fix filenotfile error

--- src/network.py
+++ src/network.py
@@ -2,33 +2,75 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.transforms as transforms
+import os
 
 
 class NeuralNet(nn.Module):
     def __init__(self):
         super().__init__()
-
-        self.conv1 = nn.Conv2d(3, 12, 5) # 32 - 5/1 = 27 + 1 = 28 -> (12, 28, 28)
-        self.pool = nn.MaxPool2d(2, 2) # (12, 14, 14)
-        self.conv2 = nn.Conv2d(12, 24, 5) # 14 - 5 / 1 + 1 = 10 -> (24, 10, 10) -> (24, 5, 5) -> Flatten (24 * 5 *5)
-        self.fc1 = nn.Linear(24 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10) # output must be 10
-
+        
+        # First block
+        self.conv1 = nn.Conv2d(3, 32, 3, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)   #add batch normalization after convulutions to stabilize learning
+        self.conv2 = nn.Conv2d(32, 32, 3, padding=1)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.pool1 = nn.MaxPool2d(2, 2)
+        self.dropout1 = nn.Dropout(0.25) #add dropout before fc to reduce overfittingg
+        
+        # Second block
+        self.conv3 = nn.Conv2d(32, 64, 3, padding=1)
+        self.bn3 = nn.BatchNorm2d(64)
+        self.conv4 = nn.Conv2d(64, 64, 3, padding=1)
+        self.bn4 = nn.BatchNorm2d(64)
+        self.pool2 = nn.MaxPool2d(2, 2)
+        self.dropout2 = nn.Dropout(0.25)
+        
+        # Third block
+        self.conv5 = nn.Conv2d(64, 128, 3, padding=1)
+        self.bn5 = nn.BatchNorm2d(128)
+        self.conv6 = nn.Conv2d(128, 128, 3, padding=1)
+        self.bn6 = nn.BatchNorm2d(128)
+        self.pool3 = nn.MaxPool2d(2, 2)
+        self.dropout3 = nn.Dropout(0.25)
+        
+        # Fully connected layers
+        self.fc1 = nn.Linear(128 * 4 * 4, 512)
+        self.bn7 = nn.BatchNorm1d(512)
+        self.dropout4 = nn.Dropout(0.5)
+        self.fc2 = nn.Linear(512, 10)
+        
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
+        # First block
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = self.pool1(x)
+        x = self.dropout1(x)
+        
+        # Second block
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = F.relu(self.bn4(self.conv4(x)))
+        x = self.pool2(x)
+        x = self.dropout2(x)
+        
+        # Third block
+        x = F.relu(self.bn5(self.conv5(x)))
+        x = F.relu(self.bn6(self.conv6(x)))
+        x = self.pool3(x)
+        x = self.dropout3(x)
+        
+        # Fully connected
         x = torch.flatten(x, 1)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = F.relu(self.bn7(self.fc1(x)))
+        x = self.dropout4(x)
+        x = self.fc2(x)
+        
         return x
-
 
 
 def image_classify(img):
     net = NeuralNet()
-    net.load_state_dict(torch.load('trained_net.pth'))
+    os.chdir('./src')
+    net.load_state_dict(torch.load('./trained_net.pth'))
 
 
     class_names = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
